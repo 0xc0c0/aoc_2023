2575c8d4127f046b40c2d0bb16a31ab8b26edaac
0xc0c0 <0xc0c0@benslab.org>
2023-12-02 05:09:16 -0600

    Day 1 Complete

--- day1/solve.py
+++ day1/solve.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import numpy as np
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
@@ -10,18 +9,60 @@ def get_file_data(fn='input.txt'):
         data = f.read()
     return data
 
+def get_cal_val(text):
+    logger.debug(text)
+    digits = [x for x in text if x.isdigit()]
+    val = int(f"{digits[0]}{digits[-1]}")
+    return val
+
+def get_cal_sum(calibration_lines):
+    logger.debug(calibration_lines)
+    return sum([get_cal_val(x) for x in calibration_lines])
+
+def get_cal_val_part2(text):
+    logger.debug(f"oldtext: {text}")
+    digits = list()
+    for i in range(len(text)):
+        if text[i].isdigit():
+            digits.append(text[i])
+        elif text[i:].find('one') == 0:
+            digits.append('1')
+        elif text[i:].find('two') == 0:
+            digits.append('2')
+        elif text[i:].find('three') == 0:       
+            digits.append('3')
+        elif text[i:].find('four') == 0:
+            digits.append('4')
+        elif text[i:].find('five') == 0:
+            digits.append('5')
+        elif text[i:].find('six') == 0:
+            digits.append('6')
+        elif text[i:].find('seven') == 0:
+            digits.append('7')
+        elif text[i:].find('eight') == 0:
+            digits.append('8')
+        elif text[i:].find('nine') == 0:
+            digits.append('9')
+    logger.debug(digits)
+    val = int(f"{digits[0]}{digits[-1]}")
+    return val
+
+def get_cal_sum_part2(calibration_lines):
+    logger.debug(calibration_lines)
+    return sum([get_cal_val_part2(x) for x in calibration_lines])
+
 def parse_data(text_data):
-    data = [[int(x) for x in list(line)] for line in text_data.strip('\n').strip().split('\n')]
-    return np.array(data)
+    data = [line.strip() for line in text_data.strip().split('\n')]
+    return data
 
 def main():
     logger.setLevel(level=logging.INFO)
     data = get_file_data()
-    model = parse_data(data)
-    answer = 0
-    print(f"Puzzle1: <SUMMARY>: {answer}")
-    answer = 0
-    print(f"Puzzle2: <SUMMARY>: {answer}")
+    calibration_lines = parse_data(data)
+    answer = get_cal_sum(calibration_lines)
+    print(f"Puzzle1: Calibration (Part 1): {answer}")
+    answer = get_cal_sum_part2(calibration_lines)
+    print(f"Puzzle2: Calibration (Part 2): {answer}")
     
 if __name__ == '__main__':
     main()
--- day1/test_solve.py
+++ day1/test_solve.py
@@ -10,12 +10,24 @@ def test_data():
         text = f.read()
     return text
 
+@pytest.fixture
+def test_data2():
+    #dynamically obtain full path of 'test.txt'
+    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test2.txt')
+    with open(test_file, 'r') as f:
+        text = f.read()
+    return text
+
 def test_parse_input(test_data):
-    grid = parse_data(test_data)
-    assert type(grid) == np.ndarray
-    assert grid[1,3] == 5
-    assert grid.ndim == 2
-    assert grid.shape == (10,10)
+    calibration_lines = parse_data(test_data)
+    assert get_cal_val(calibration_lines[0]) == 12
+    assert get_cal_val(calibration_lines[3]) == 77
+    
+def test_parse_input_2(test_data2):
+    calibration_lines = parse_data(test_data2)
+    assert get_cal_val_part2(calibration_lines[0]) == 29
+    assert get_cal_val_part2(calibration_lines[3]) == 24
+    
   
 def test_all(test_data):
     grid = parse_data(test_data)
