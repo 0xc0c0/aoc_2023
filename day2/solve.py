76d81f335971bfcff1234c080da57212a89d50b3
0xc0c0 <0xc0c0@benslab.org>
2023-12-02 07:41:50 -0600

    Completed Day 2

--- day2/solve.py
+++ day2/solve.py
@@ -1,33 +1,137 @@
-import logging
+#!/usr/bin/env python
+
+"""Python solver file for Advent of Code Day 2"""
 import os
-import numpy as np
+import logging
+import math
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
-def get_file_data(fn='input.txt'):
-    with open(fn) as f:
+
+def get_file_data(fn="input.txt"):
+    """Function returning data blob from input.txt file"""
+    with open(fn, "r", encoding="utf-8") as f:
         data = f.read()
     return data
 
+
+def parse_bag(bag_text):
+    """Parse bag within a round
+
+    Args:
+        bag_text (str): bag text
+
+    Returns:
+        dict: color (key) and cube count (value)
+    """
+    cube_count, bag_color = bag_text.strip().split()
+    cube_count = int(cube_count)
+    return (bag_color, cube_count)
+
+
+def parse_round(round_text):
+    """Parse round within the text list of rounds
+
+    Args:
+        round_text (str): round text
+
+    Returns:
+        list: parsed bags composing the round
+    """
+    bags = [parse_bag(x) for x in round_text.strip().split(",")]
+    bags = {k: v for (k, v) in bags}
+    return bags
+
+
+def parse_line(text_line):
+    """Parses single line of text from the input file
+
+    Args:
+        text_line (str): raw text line from input file
+    """
+    # logger.debug("split text_line is %s", text_line.strip().split(":"))
+    # logger.debug("length: %d", len(text_line.strip().split(":")))
+    game_text, rounds_text = text_line.strip().split(":")
+    game_index = int(game_text.strip().split()[1])
+    rounds = [parse_round(round_text) for round_text in rounds_text.strip().split(";")]
+    return (game_index, rounds)
+
+
 def parse_data(text_data):
-    data = [[int(x) for x in list(line)] for line in text_data.strip('\n').strip().split('\n')]
-    return np.array(data)
+    """_summary_
 
-def function(param):
-    return 0
+    Args:
+        text_data (str): raw text blob from input file
 
-def function2(param):
-    return 0
+    Returns:
+        _type_: parsed input data ready for processing
+    """
+    data = [parse_line(line) for (line) in text_data.strip().strip("\n").split("\n")]
+    new_data = {k: v for (k, v) in data}
+    logger.debug(new_data)
+    return new_data
+
+
+def get_possible_games_sum(data):
+    """Complete Part 1 work
+
+    Args:
+        data (list): list of parsed input objects/dictionaries
+
+    Returns:
+        int: answer to Part 1 question
+    """
+    maxes = {"red": 12, "green": 13, "blue": 14}
+
+    all_games = set(data.keys())
+    impossible = set()
+
+    for game_number, game_rounds in data.items():
+        for game_round in game_rounds:
+            for bag_color, cubes in game_round.items():
+                if cubes > maxes[bag_color]:
+                    impossible.add(game_number)
+
+    possible = all_games - impossible
+    return sum(possible)
+
+
+def get_power(game_rounds):
+    maxes = {"red": 0, "green": 0, "blue": 0}
+
+    for game_round in game_rounds:
+        for bag_color, cubes in game_round.items():
+            if cubes > maxes[bag_color]:
+                maxes[bag_color] = cubes
+
+    return math.prod(maxes.values())
+
+
+def get_min_power_sum(data):
+    """Complete Part 2 work
+
+    Args:
+        data (list): list of parsed input objects/dictionaries
+
+    Returns:
+        int: answer to Part 2 question
+    """
+
+    powers = [get_power(v) for (k, v) in data.items()]
+    return sum(powers)
+
 
 def main():
+    """Main function used to solve AoC problem"""
     logger.setLevel(level=logging.INFO)
-    data = get_file_data()
-    model = parse_data(data)
-    answer = 0
-    print(f"Puzzle1: <SUMMARY>: {answer}")
-    answer = 0
-    print(f"Puzzle2: <SUMMARY>: {answer}")
-    
-if __name__ == '__main__':
+    text_data = get_file_data()
+    data = parse_data(text_data)
+    answer = get_possible_games_sum(data)
+    print(f"Day 2: Part 1: Get Possible Games: {answer}")
+    answer2 = get_min_power_sum(data)
+    print(f"Day 2: Part 2: Get Sum of Powers: {answer2}")
+
+
+if __name__ == "__main__":
     main()
--- day2/test_solve.py
+++ day2/test_solve.py
@@ -1,33 +1,59 @@
-import pytest
+"""Python test file for unit testing in support of AoC solves"""
 import os
-from .solve import *
+import pytest
+from .solve import parse_data, get_possible_games_sum, get_min_power_sum
 
-@pytest.fixture
-def test_data():
-    #dynamically obtain full path of 'test.txt'
-    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.txt')
-    with open(test_file, 'r') as f:
+
+@pytest.fixture(name="test_data")
+def get_test_data_1():
+    """Provides test data using text.txt for return of file to consume
+
+    Returns:
+        str: data blob of text from file
+    """
+    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test.txt")
+    with open(test_file, "r", encoding="utf-8") as f:
         text = f.read()
     return text
 
-@pytest.fixture
-def test_data2():
-    #dynamically obtain full path of 'test.txt'
-    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test2.txt')
-    with open(test_file, 'r') as f:
+
+@pytest.fixture(name="test_data2")
+def get_test_data_2():
+    """Provides test data using text2.txt for return of file to consume
+
+    Returns:
+        str: data blob of text from file
+    """
+    # dynamically obtain full path of 'test.txt'
+    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test2.txt")
+    with open(test_file, "r", encoding="utf-8") as f:
         text = f.read()
     return text
 
-def test_parse_input(test_data):
-    calibration_lines = parse_data(test_data)
-    assert function(calibration_lines[0]) == 0
-    assert function(calibration_lines[3]) == 0
-    
-def test_parse_input_2(test_data2):
-    calibration_lines = parse_data(test_data2)
-    assert function2(calibration_lines[0]) == 0
-    assert function2(calibration_lines[3]) == 0
-    
-  
-def test_all(test_data):
-    grid = parse_data(test_data)
+
+def test_parse_input(test_data, test_data2):
+    """Test all parsing functions associated with Part 1
+
+    Args:
+        test_data (str): takes in a raw text str object as a data blob
+    """
+    data = parse_data(test_data)
+    assert isinstance(data, dict)
+    assert data[1][0]["blue"] == 3
+
+    # data = parse_data(test_data2)
+    # assert (len(data[0])) == 0
+
+
+def test_all(test_data, test_data2):
+    """Test all functions associated with Parts 1 and 2
+
+    Args:
+        test_data (str): takes in a raw text str object as a data blob
+    """
+    data = parse_data(test_data)
+    assert get_possible_games_sum(data) == 8
+    assert get_min_power_sum(data) == 2286
+
+    # data = parse_data(test_data2)
+    # assert function2(data) == 0
