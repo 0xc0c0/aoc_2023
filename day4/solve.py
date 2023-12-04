4513c85e3a3ee64856f37a04ecd29ff96e40e69f
0xc0c0 <0xc0c0@benslab.org>
2023-12-03 23:48:23 -0600

    Completed Day 4

--- day4/solve.py
+++ day4/solve.py
@@ -21,7 +21,12 @@ def parse_line(text_line):
     Args:
         text_line (str): raw text line from input file
     """
-    return list(text_line)
+    text_left, text_right = text_line.strip().split("|")
+    card_num_text, win_text = text_left.strip().split(":")
+    card_num = int(card_num_text.split()[1].strip())
+    winning_nums = [int(x) for x in win_text.strip().split()]
+    drawn_nums = [int(x) for x in text_right.strip().split()]
+    return card_num, winning_nums, drawn_nums
 
 
 def parse_data(text_data):
@@ -33,11 +38,13 @@ def parse_data(text_data):
     Returns:
         _type_: parsed input data ready for processing
     """
-    data = [parse_line(line.strip()) for line in text_data.strip().split("\n")]
-    return data
+    cards = [
+        parse_line(line.strip()) for line in text_data.strip("\n").strip().split("\n")
+    ]
+    return cards
 
 
-def function(data):
+def get_points_worth(cards):
     """Complete Part 1 work
 
     Args:
@@ -46,10 +53,19 @@ def function(data):
     Returns:
         int: answer to Part 1 question
     """
-    return 0
+    total = 0
+    for _, winning_nums, drawn_nums in cards:
+        card_winners = 0
+        for w in winning_nums:
+            if w in drawn_nums:
+                card_winners += 1
+        if card_winners > 0:
+            total += 2 ** (card_winners - 1)
+
+    return total
 
 
-def function2(data):
+def get_cards_count(cards):
     """Complete Part 2 work
 
     Args:
@@ -58,7 +74,29 @@ def function2(data):
     Returns:
         int: answer to Part 2 question
     """
-    return 0
+
+    cards_to_process = [1] * (len(cards) + 1)
+    cards_to_process[0] = 0
+    winners_count = [0] * (len(cards) + 1)
+    total_cards = 0
+
+    for card_num, winning_nums, drawn_nums in cards:
+        card_winners = 0
+        for w in winning_nums:
+            if w in drawn_nums:
+                card_winners += 1
+        winners_count[card_num] = card_winners
+
+    for i, _ in enumerate(cards_to_process):
+        logger.debug("processing card index %d", i)
+        while cards_to_process[i] > 0:
+            # process a card
+            total_cards += 1
+            cards_to_process[i] -= 1
+            for a in range(winners_count[i]):
+                # add a card to process at the offset for the winner
+                cards_to_process[i + a + 1] += 1
+    return total_cards
 
 
 def main():
@@ -66,10 +104,10 @@ def main():
     logger.setLevel(level=logging.INFO)
     text_data = get_file_data()
     data = parse_data(text_data)
-    answer = function(data)
-    print(f"Day 4: Part 1: <SUMMARY>: {answer}")
-    answer2 = function2(data)
-    print(f"Day 4: Part 2: <SUMMARY>: {answer2}")
+    answer = get_points_worth(data)
+    print(f"Day 4: Part 1: Total Scratchcard Points: {answer}")
+    answer2 = get_cards_count(data)
+    print(f"Day 4: Part 2: Total Scratchcards: {answer2}")
 
 
 if __name__ == "__main__":
--- day4/test_solve.py
+++ day4/test_solve.py
@@ -1,7 +1,7 @@
 """Python test file for unit testing in support of AoC solves"""
 import os
 import pytest
-from .solve import parse_data, function, function2
+from .solve import parse_data, get_points_worth, get_cards_count
 
 
 @pytest.fixture(name="test_data")
@@ -17,41 +17,25 @@ def get_test_data_1():
     return text
 
 
-@pytest.fixture(name="test_data2")
-def get_test_data_2():
-    """Provides test data using text2.txt for return of file to consume
-
-    Returns:
-        str: data blob of text from file
-    """
-    # dynamically obtain full path of 'test.txt'
-    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test2.txt")
-    with open(test_file, "r", encoding="utf-8") as f:
-        text = f.read()
-    return text
-
-
-def test_parse_input(test_data, test_data2):
+def test_parse_input(test_data):
     """Test all parsing functions associated with Part 1
 
     Args:
         test_data (str): takes in a raw text str object as a data blob
     """
     data = parse_data(test_data)
-    assert (len(data[0])) == 0
+    assert (len(data)) == 6
+    assert data[0][0] == 1  # card 1
+    assert data[2][0] == 3  # card 3
+    assert data[2][1][2] == 53  # 3rd winning number on card 3
 
-    data = parse_data(test_data2)
-    assert (len(data[0])) == 0
 
-
-def test_all(test_data, test_data2):
+def test_all(test_data):
     """Test all functions associated with Parts 1 and 2
 
     Args:
         test_data (str): takes in a raw text str object as a data blob
     """
     data = parse_data(test_data)
-    assert function(data) == 0
-
-    data = parse_data(test_data2)
-    assert function2(data) == 0
+    assert get_points_worth(data) == 13
+    assert get_cards_count(data) == 30
