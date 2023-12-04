206ec04ac38ad8aec4ce004adf6afc298af14a60
0xc0c0 <0xc0c0@benslab.org>
2023-12-03 23:49:40 -0600

    updated template

--- template/test_solve.py
+++ template/test_solve.py
@@ -17,34 +17,31 @@ def get_test_data_1():
     return text
 
 
-@pytest.fixture(name="test_data2")
-def get_test_data_2():
-    """Provides test data using text2.txt for return of file to consume
+# @pytest.fixture(name="test_data2")
+# def get_test_data_2():
+#     """Provides test data using text2.txt for return of file to consume
 
-    Returns:
-        str: data blob of text from file
-    """
-    # dynamically obtain full path of 'test.txt'
-    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test2.txt")
-    with open(test_file, "r", encoding="utf-8") as f:
-        text = f.read()
-    return text
+#     Returns:
+#         str: data blob of text from file
+#     """
+#     # dynamically obtain full path of 'test.txt'
+#     test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test2.txt")
+#     with open(test_file, "r", encoding="utf-8") as f:
+#         text = f.read()
+#     return text
 
 
-def test_parse_input(test_data, test_data2):
+def test_parse_input(test_data):
     """Test all parsing functions associated with Part 1
 
     Args:
         test_data (str): takes in a raw text str object as a data blob
     """
     data = parse_data(test_data)
-    assert (len(data[0])) == 0
-
-    data = parse_data(test_data2)
-    assert (len(data[0])) == 0
+    assert (len(data)) == 0
 
 
-def test_all(test_data, test_data2):
+def test_all(test_data):
     """Test all functions associated with Parts 1 and 2
 
     Args:
@@ -52,6 +49,3 @@ def test_all(test_data, test_data2):
     """
     data = parse_data(test_data)
     assert function(data) == 0
-
-    data = parse_data(test_data2)
-    assert function2(data) == 0
