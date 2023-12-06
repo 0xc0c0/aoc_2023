120fe9cea450872f93aab13608ec9e70d5153f15
0xc0c0 <0xc0c0@benslab.org>
2023-12-05 23:36:04 -0600

    Completed Day 6

--- day6/solve.py
+++ day6/solve.py
@@ -2,6 +2,7 @@
 
 """Python solver file for Advent of Code Day 6"""
 import os
+import math
 import logging
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
@@ -15,13 +16,13 @@ def get_file_data(fn="input.txt"):
     return data
 
 
-def parse_line(text_line):
-    """Parses single line of text from the input file
+# def parse_line(text_line):
+#     """Parses single line of text from the input file
 
-    Args:
-        text_line (str): raw text line from input file
-    """
-    return list(text_line)
+#     Args:
+#         text_line (str): raw text line from input file
+#     """
+#     return list(text_line)
 
 
 def parse_data(text_data):
@@ -33,11 +34,45 @@ def parse_data(text_data):
     Returns:
         _type_: parsed input data ready for processing
     """
-    data = [parse_line(line.strip()) for line in text_data.strip().split("\n")]
-    return data
+    text_time, text_distance = text_data.strip().strip("\n").split("\n")
+    times = [int(t) for t in text_time.split(":")[1].strip().split()]
+    distances = [int(d) for d in text_distance.split(":")[1].strip().split()]
+    return times, distances
 
 
-def function(data):
+def parse_data2(text_data):
+    """Parses full data input
+
+    Args:
+        text_data (str): raw text blob from input file
+
+    Returns:
+        _type_: parsed input data ready for processing
+    """
+    text_time, text_distance = text_data.strip().strip("\n").split("\n")
+    time = int(text_time.split(":")[1].strip().replace(" ", ""))
+    distance = int(text_distance.split(":")[1].strip().replace(" ", ""))
+    return time, distance
+
+
+def get_race_winners(time, distance):
+    """Find race winners for a (time, distance) race
+
+    Args:
+        time (int): time alloted for race in ms
+        distance (int): current record distance for race
+
+    Returns:
+        int: possible unique whole number (ms) charge times that result in a record breaker
+    """
+    count = 0
+    for s in range(time):
+        if s * (time - s) > distance:
+            count += 1
+    return count
+
+
+def find_wins(times, distances):
     """Complete Part 1 work
 
     Args:
@@ -46,19 +81,8 @@ def function(data):
     Returns:
         int: answer to Part 1 question
     """
-    return 0
-
-
-def function2(data):
-    """Complete Part 2 work
-
-    Args:
-        data (list): list of parsed input objects/dictionaries
-
-    Returns:
-        int: answer to Part 2 question
-    """
-    return 0
+    counts = [get_race_winners(t, d) for t, d in zip(times, distances)]
+    return math.prod(counts)
 
 
 def main():
@@ -66,10 +90,11 @@ def main():
     logger.setLevel(level=logging.INFO)
     text_data = get_file_data()
     data = parse_data(text_data)
-    answer = function(data)
-    print(f"Day 6: Part 1: <SUMMARY>: {answer}")
-    answer2 = function2(data)
-    print(f"Day 6: Part 2: <SUMMARY>: {answer2}")
+    answer = find_wins(*data)
+    print(f"Day 6: Part 1: Find possible winners: {answer}")
+    data = parse_data2(text_data)
+    answer2 = get_race_winners(*data)
+    print(f"Day 6: Part 2: Find possible winners (large number): {answer2}")
 
 
 if __name__ == "__main__":
--- day6/test_solve.py
+++ day6/test_solve.py
@@ -1,7 +1,7 @@
 """Python test file for unit testing in support of AoC solves"""
 import os
 import pytest
-from .solve import parse_data, function, function2
+from .solve import parse_data, parse_data2, find_wins, get_race_winners
 
 
 @pytest.fixture(name="test_data")
@@ -38,7 +38,9 @@ def test_parse_input(test_data):
         test_data (str): takes in a raw text str object as a data blob
     """
     data = parse_data(test_data)
-    assert (len(data)) == 0
+    times, distances = data
+    assert times[0] == 7
+    assert len(times) == len(distances)
 
 
 def test_all(test_data):
@@ -48,4 +50,7 @@ def test_all(test_data):
         test_data (str): takes in a raw text str object as a data blob
     """
     data = parse_data(test_data)
-    assert function(data) == 0
+    assert find_wins(*data) == 288
+
+    data = parse_data2(test_data)
+    assert get_race_winners(*data) == 71503
