43371513c073899beb782a8d6b230b8b62ca5d3d
0xc0c0 <0xc0c0@benslab.org>
2023-12-16 00:01:38 -0600

    Initial Day 17

---
+++ day17/solve.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+"""Python solver file for Advent of Code Day 17"""
+import os
+import logging
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logger = logging.getLogger(__name__)
+
+
+def get_file_data(fn="input.txt"):
+    """Function returning data blob from input.txt file"""
+    with open(fn, "r", encoding="utf-8") as f:
+        data = f.read()
+    return data
+
+
+def parse_line(text_line):
+    """Parses single line of text from the input file
+
+    Args:
+        text_line (str): raw text line from input file
+    """
+    return list(text_line)
+
+
+def parse_data(text_data):
+    """Parses full data input
+
+    Args:
+        text_data (str): raw text blob from input file
+
+    Returns:
+        _type_: parsed input data ready for processing
+    """
+    data = [
+        parse_line(line.strip()) for line in text_data.strip().strip("\n").split("\n")
+    ]
+    return data
+
+
+def function(data):
+    """Complete Part 1 work
+
+    Args:
+        data (list): list of parsed input objects/dictionaries
+
+    Returns:
+        int: answer to Part 1 question
+    """
+    return 0
+
+
+def function2(data):
+    """Complete Part 2 work
+
+    Args:
+        data (list): list of parsed input objects/dictionaries
+
+    Returns:
+        int: answer to Part 2 question
+    """
+    return 0
+
+
+def main():
+    """Main function used to solve AoC problem"""
+    logger.setLevel(level=logging.INFO)
+    text_data = get_file_data()
+    data = parse_data(text_data)
+    answer = function(data)
+    print(f"Day 17: Part 1: <SUMMARY>: {answer}")
+    answer2 = function2(data)
+    print(f"Day 17: Part 2: <SUMMARY>: {answer2}")
+
+
+if __name__ == "__main__":
+    main()
