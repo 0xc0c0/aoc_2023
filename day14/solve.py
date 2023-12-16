5001e1465fd87374f9f472e3f358582fd0fb8ac4
0xc0c0 <0xc0c0@benslab.org>
2023-12-15 21:26:20 -0600

    Completed Day 14

--- day14/solve.py
+++ day14/solve.py
@@ -3,6 +3,7 @@
 """Python solver file for Advent of Code Day 14"""
 import os
 import logging
+import numpy as np
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
@@ -21,7 +22,8 @@ def parse_line(text_line):
     Args:
         text_line (str): raw text line from input file
     """
-    return list(text_line)
+    lookup = {"O": 2, "#": 1, ".": 0}
+    return [lookup[x] for x in list(text_line)]
 
 
 def parse_data(text_data):
@@ -36,22 +38,120 @@ def parse_data(text_data):
     data = [
         parse_line(line.strip()) for line in text_data.strip().strip("\n").split("\n")
     ]
-    return data
+    return np.array(data)
 
 
-def function(data):
+def get_load_after_north_tilt(platform):
     """Complete Part 1 work
 
     Args:
-        data (list): list of parsed input objects/dictionaries
+        platform (np.array): grid of values for the rocks
 
     Returns:
         int: answer to Part 1 question
     """
-    return 0
+    platform = platform.copy()
+    for c in range(platform.shape[1]):
+        for base_r in range(platform.shape[0] - 1):
+            # has to be a '.' to be replaceable
+            if platform[base_r, c] == 0:
+                for check_r in range(base_r + 1, platform.shape[0]):
+                    # if you reach a square rock, stop
+                    if platform[check_r, c] == 1:
+                        break
+                    if platform[check_r, c] == 2:
+                        # swap and break
+                        platform[base_r, c] = 2
+                        platform[check_r, c] = 0
+                        break
+
+    load = 0
+    for r in range(platform.shape[0]):
+        load += np.sum(platform[r, :] == 2) * (platform.shape[0] - r)
+
+    return load
 
 
-def function2(data):
+def run_spin_cycle(platform):
+    """_summary_
+
+    Args:
+        platform (_type_): _description_
+    """
+    # north tilt
+    for c in range(platform.shape[1]):
+        for base_r in range(platform.shape[0] - 1):
+            # has to be a '.' to be replaceable
+            if platform[base_r, c] == 0:
+                for check_r in range(base_r + 1, platform.shape[0]):
+                    # if you reach a square rock, stop
+                    if platform[check_r, c] == 1:
+                        break
+                    if platform[check_r, c] == 2:
+                        # swap and break
+                        platform[base_r, c] = 2
+                        platform[check_r, c] = 0
+                        break
+
+    # west
+    for r in range(platform.shape[0]):
+        for base_c in range(platform.shape[1] - 1):
+            # has to be a '.' to be replaceable
+            if platform[r, base_c] == 0:
+                for check_c in range(base_c + 1, platform.shape[1]):
+                    # if you reach a square rock, stop
+                    if platform[r, check_c] == 1:
+                        break
+                    if platform[r, check_c] == 2:
+                        # swap and break
+                        platform[r, base_c] = 2
+                        platform[r, check_c] = 0
+                        break
+    # south tilt
+    for c in range(platform.shape[1]):
+        for base_r in range(platform.shape[0] - 1, 0, -1):
+            # has to be a '.' to be replaceable
+            if platform[base_r, c] == 0:
+                for check_r in range(base_r - 1, -1, -1):
+                    # if you reach a square rock, stop
+                    if platform[check_r, c] == 1:
+                        break
+                    if platform[check_r, c] == 2:
+                        # swap and break
+                        platform[base_r, c] = 2
+                        platform[check_r, c] = 0
+                        break
+    # east tilt
+    for r in range(platform.shape[0]):
+        for base_c in range(platform.shape[1] - 1, 0, -1):
+            # has to be a '.' to be replaceable
+            if platform[r, base_c] == 0:
+                for check_c in range(base_c - 1, -1, -1):
+                    # if you reach a square rock, stop
+                    if platform[r, check_c] == 1:
+                        break
+                    if platform[r, check_c] == 2:
+                        # swap and break
+                        platform[r, base_c] = 2
+                        platform[r, check_c] = 0
+                        break
+
+    return platform
+
+
+def get_load(platform):
+    """_summary_
+
+    Args:
+        platform (_type_): _description_
+    """
+    load = 0
+    for r in range(platform.shape[0]):
+        load += np.sum(platform[r, :] == 2) * (platform.shape[0] - r)
+    return load
+
+
+def get_load_after_spin_cycles(platform, cycles=1000000000):
     """Complete Part 2 work
 
     Args:
@@ -60,7 +160,29 @@ def function2(data):
     Returns:
         int: answer to Part 2 question
     """
-    return 0
+    init_qty = 500
+    platform = platform.copy()
+
+    # find pattern
+    loads = []
+    i = 0
+    no_found_repeat = True
+    while len(loads) < init_qty and no_found_repeat:
+        i_load = get_load(platform)
+        logger.debug("load after %d cycles: %d", i, i_load)
+        loads.append(i_load)
+        i += 1
+        platform = run_spin_cycle(platform)
+
+    # find cycle length
+    i = 100
+    while loads[-i:] != loads[-i * 2 : -i]:
+        i += 1
+
+    # at this point, we know the end of the pattern works in multiples of i
+    repeat_end = loads[-i:]
+    end_value = repeat_end[(cycles - init_qty) % i]
+    return end_value
 
 
 def main():
@@ -68,10 +190,10 @@ def main():
     logger.setLevel(level=logging.INFO)
     text_data = get_file_data()
     data = parse_data(text_data)
-    answer = function(data)
-    print(f"Day 14: Part 1: <SUMMARY>: {answer}")
-    answer2 = function2(data)
-    print(f"Day 14: Part 2: <SUMMARY>: {answer2}")
+    answer = get_load_after_north_tilt(data)
+    print(f"Day 14: Part 1: Get Load After North Tilt: {answer}")
+    answer2 = get_load_after_spin_cycles(data)
+    print(f"Day 14: Part 2: Get Load After 1000000000 Spins: {answer2}")
 
 
 if __name__ == "__main__":
